4cffd41cbbfc5a011ae81b3c8cc4143e0adfbabf
Calista Pemberton <minervamachina@gmail.com>
2022-05-05 16:03:00 +0800

    input masking for expiration date

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms' 
 
+import { DateFormControl } from '../date-form-control'
+
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
@@ -18,7 +20,7 @@ export class CardFormComponent implements OnInit {
       Validators.minLength(16),
       Validators.maxLength(16)
     ]),
-    expiration: new FormControl('', [
+    expiration: new DateFormControl('', [
       Validators.required,
       Validators.pattern(/^(0[0-9]|1[0-2])\/\d{2}$/)
     ]),
