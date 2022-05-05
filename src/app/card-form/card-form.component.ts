001af82599c445ae664d9ffa05db5f68a141ffe5
Calista Pemberton <minervamachina@gmail.com>
2022-05-05 15:30:14 +0800

    add error styling

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -11,6 +11,7 @@ export class CardFormComponent implements OnInit {
     name: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
+      Validators.pattern('[a-zA-Z ]*')
     ])
   })
   constructor() { 
